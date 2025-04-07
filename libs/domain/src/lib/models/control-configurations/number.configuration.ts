933482999e029dea4c2ee04754c3a200368f012c
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-07 10:41:50 -0500

    Create number.configuration.ts

---
+++ libs/domain/src/lib/models/control-configurations/number.configuration.ts
@@ -0,0 +1,12 @@
+export interface NumberConfiguration {
+  decimalPlaces: number;
+  isCurrency: boolean;
+  min?: number;
+  max?: number;
+  placeholder?: string;
+  step?: number;
+}
+
+export function getDefaultNumberConfiguration(overrides: Partial<NumberConfiguration>): NumberConfiguration {
+  return Object.assign({ decimalPlaces: 0, isCurrency: false, min: 0, max: undefined, step: 1 }, overrides);
+}
