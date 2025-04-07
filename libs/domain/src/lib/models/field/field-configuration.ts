f3b3f68cff8f87b5ad81c044c81500e614de5129
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-07 10:38:12 -0500

    Update field-configuration.ts
    Add events

--- libs/domain/src/lib/models/field/field-configuration.ts
+++ libs/domain/src/lib/models/field/field-configuration.ts
@@ -8,7 +8,7 @@ export interface FieldConfiguration<T> {
   controlConfig: T;
   dataPath: string[];
   name: string;
-  value: FieldValue;
+  value?: FieldValue;
   label?: string;
   isRequired?: boolean;
   isDisabled?: boolean;
@@ -16,15 +16,34 @@ export interface FieldConfiguration<T> {
   hideLabel?: boolean;
   hint?: string;
   validators?: Validator[];
+
+  onChanged?: (event: any) => void;
+  onFocused?: (event: any) => void;
+  onBlurred?: (event: any) => void;
 }
 
-export function getDefaultFieldConfiguration<T>(overrides?: Partial<FieldConfiguration<T>>): FieldConfiguration<T> {
+export function getDefaultFieldConfiguration<T>(overrides: Partial<FieldConfiguration<T>>): FieldConfiguration<T> {
+    if(!overrides.inputId) {
+    throw new Error(`[getDefaultFieldConfiguration] inputId is a required field. Making config for ${overrides.name}`);
+  }
+  if(!overrides.name) {
+    throw new Error(`[getDefaultFieldConfiguration] name is a required field. Making config for ${overrides.inputId}`);
+  }
+  if(!overrides.type) {
+    throw new Error(`[getDefaultFieldConfiguration] type is a required field. Making config for ${overrides.inputId}`);
+  }
+  if(!overrides.controlConfig) {
+    throw new Error(`[getDefaultFieldConfiguration] controlConfig is a required field. Making config for ${overrides.inputId}`);
+  }
+  if(!overrides.dataPath?.length) {
+    throw new Error(`[getDefaultFieldConfiguration] dataPath is a required field. Making config for ${overrides.inputId}`);
+  }
+  
     return Object.assign({
         id: '',
-        type: FieldType.checkbox,
+        type: overrides?.type,
         controlConfig: {} as T,
         dataPath: [],
-        name: '',
-        value: ''
+        name: ''
     }, overrides);
 }
