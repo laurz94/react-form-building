388a22d50a0ddcf8fe5cb88f211612f6f0b8f79b
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-10 17:32:49 -0500

    Create readonly-field.configuration.ts

---
+++ libs/domain/src/lib/models/control-configurations/readonly-field.configuration.ts
@@ -0,0 +1,9 @@
+export interface ReadonlyFieldConfiguration {
+    inputId?: string;
+    label: string;
+    value: any;
+    isCurrency?: boolean;
+    hideLabel?: boolean;
+    hint?: string;
+    valueClassName?: string;
+}
