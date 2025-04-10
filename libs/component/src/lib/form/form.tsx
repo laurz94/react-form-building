4bd02fedd8b076996d1790691f3e5c11eb3a6518
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-10 17:20:29 -0500

    Update form.tsx

--- libs/component/src/lib/form/form.tsx
+++ libs/component/src/lib/form/form.tsx
@@ -1,11 +1,122 @@
-import styles from './form.module.css';
+import {
+  BaseFieldConfiguration,
+  FieldConfiguration,
+  FieldRule,
+  FieldRuleUpdateEvent,
+  processFieldRules,
+  ReadonlyFieldConfiguration,
+} from '@libs/domain';
+import { useState } from 'react';
+import Field from '../field/field';
+import { ReadonlyField } from '../readonly-field';
+import Section from '../section/section';
+import { SectionConfiguration } from '../section/section.configuration';
 
-export function Form() {
+export function LibForm({
+  fields,
+  fieldRules,
+  sectionConfig,
+  onBlurred,
+  onChanged,
+  onFocused,
+}: {
+  fields: (
+    | FieldConfiguration<BaseFieldConfiguration>
+    | SectionConfiguration
+    | ReadonlyFieldConfiguration
+  )[];
+  fieldRules?: FieldRule[];
+  sectionConfig?: SectionConfiguration;
+  onBlurred?: (value: string | number) => void;
+  onChanged?: (value: string | number, fieldName: string) => void;
+  onFocused?: (value: string | number) => void;
+}) {
+  const [configs, setConfigs] = useState(fields);
+
+  const handleBlur = (value: any, fieldName: string) => {
+    // TODO: If we have cross field validations, it should happen here
+    // validate(value);
+    processRules(value, fieldName, 'blur');
+
+    if (onBlurred) {
+      onBlurred(value);
+    }
+  };
+
+  const handleChange = (value: any, fieldName: string) => {
+    processRules(value, fieldName, 'change');
+
+    if (onChanged) {
+      onChanged(value, fieldName);
+    }
+  };
+
+  function processRules(
+    value: any,
+    fieldName: string,
+    action: FieldRuleUpdateEvent
+  ) {
+    if (fieldRules?.length) {
+      const newConfigs = processFieldRules(
+        { fieldName, value, action: action },
+        configs as FieldConfiguration<any>[],
+        fieldRules
+      );
+
+      setConfigs(newConfigs);
+    }
+  }
+
+  const isSection = (
+    field:
+      | FieldConfiguration<any>
+      | ReadonlyFieldConfiguration
+      | SectionConfiguration
+  ): boolean => {
+    return !!(field as any).fields?.length;
+  };
+
+  const renderFields = (
+    fieldConfigs: (
+      | FieldConfiguration<any>
+      | ReadonlyFieldConfiguration
+      | SectionConfiguration
+    )[]
+  ) => {
+    return fieldConfigs.map((field) => {
+      return isSection(field) ? (
+        <Section
+          key={(field as SectionConfiguration).inputId}
+          {...(field as SectionConfiguration)}
+        >
+          {renderFields((field as any as SectionConfiguration).fields!)}
+        </Section>
+      ) : (field as FieldConfiguration<any>).controlConfig?.inputId ? (
+        <Field
+          key={
+            (field as FieldConfiguration<any>).controlConfig?.inputId ??
+            (field as any as ReadonlyFieldConfiguration).inputId
+          }
+          config={field as FieldConfiguration<any>}
+          onBlurred={handleBlur}
+          onChanged={handleChange}
+        ></Field>
+      ) : (
+        <ReadonlyField
+          {...(field as ReadonlyFieldConfiguration)}
+        ></ReadonlyField>
+      );
+    });
+  };
+
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Form!</h1>
-    </div>
+    <form className="form">
+      {sectionConfig ? (
+        <Section {...sectionConfig}>{renderFields(configs)}</Section>
+      ) : (
+        renderFields(configs)
+      )}
+    </form>
   );
 }
-
-export default Form;
+export default LibForm;
