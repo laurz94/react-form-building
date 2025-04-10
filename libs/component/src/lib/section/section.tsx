b9bc0294ef471c10a960e546df403828a5aa066a
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-10 17:24:14 -0500

    Update section.tsx

--- libs/component/src/lib/section/section.tsx
+++ libs/component/src/lib/section/section.tsx
@@ -1,10 +1,66 @@
+import { useState } from 'react';
 import styles from './section.module.css';
 
-export function Section() {
+export interface SectionConfiguration {
+  inputId: string;
+  title: string;
+  fields?: (FieldConfiguration<any> | SectionConfiguration | ReadonlyFieldConfiguration)[];
+  fieldRules?: FieldRule[];
+  children?: ReactNode;
+  icon?: string;
+  collapsable?: boolean;
+  isCollapsed?: boolean;
+  isSubSection?: boolean;
+
+  onChanged?: (value: string | number, fieldName: string) => void;
+}
+
+export function Section({
+  inputId,
+  title,
+  fields,
+  fieldRules,
+  children,
+  icon,
+  collapsable,
+  isCollapsed,
+  isSubSection,
+  onChanged,
+}: SectionConfiguration) {
+  const [collapsed, setCollapsed] = useState(isCollapsed);
+  const [toggleIcon, setToggleIcon] = useState(
+    isCollapsed ? 'fa-solid fa-plus ' : 'fa-solid fa-minus '
+  );
+  const toggle = () => {
+    const newValue = !collapsed;
+    setCollapsed(newValue);
+    setToggleIcon(newValue ? 'fa-solid fa-plus ' : 'fa-solid fa-minus ');
+  };
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Section!</h1>
-    </div>
+    <section
+      key={inputId}
+      id={inputId + '-section'}
+      className={isSubSection ? styles['sub-section'] : styles['section']}
+    >
+      {!isSubSection && (
+        <header className={styles['header']}>
+          {icon && <i className={styles['icon'] + ' ' + icon}></i>}
+          <div className={styles['title']}>{title}</div>
+          {collapsable && (
+            <button
+              className={styles['toggle']}
+              id="collapse-toggle-button"
+              onClick={toggle}
+            >
+              <i className={toggleIcon + styles['icon']}></i>
+            </button>
+          )}
+        </header>
+      )}
+      <div className={styles['body']}>
+        {(!collapsable || !collapsed) && <>{children}</>}
+      </div>
+    </section>
   );
 }
 
