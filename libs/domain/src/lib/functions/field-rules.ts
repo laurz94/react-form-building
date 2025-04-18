abe9e0a3a65f7a5eaf33705a4218bf0fee146180
Laura Slocum <lauraslocum1027@gmail.com>
2025-04-18 15:35:14 -0500

    Update field-rules.ts

--- libs/domain/src/lib/functions/field-rules.ts
+++ libs/domain/src/lib/functions/field-rules.ts
@@ -1,68 +1,120 @@
 import {
-  CheckboxConfiguration,
-  DatePickerConfiguration,
-  DropdownConfiguration,
+  BaseFieldConfiguration,
   FieldConfiguration,
   FieldRule,
   FieldRuleAction,
   FieldRuleProcessEvent,
   FormFieldType,
-  NumberConfiguration,
-  TextboxConfiguration,
+  ReadonlyFieldConfiguration,
+  SectionConfiguration,
 } from '../models';
 import { isFunction } from './object-manipulation';
 
 export function processFieldRules(
   event: FieldRuleProcessEvent,
-  configs: FormFieldType[],
+  fieldConfigs: FormFieldType[],
   fieldRules: FieldRule[],
   data?: any
 ): FormFieldType[] {
-  let newConfigs = [...configs];
+  let newFieldConfigs: FormFieldType[] = [...fieldConfigs];
 
   fieldRules
     .filter((r) => r.fieldName === event.fieldName)
     .forEach((fieldRule) => {
-      if (
-        event.value === fieldRule.valueToMatch &&
-        fieldRule.updateOn === event.action
-      ) {
-        newConfigs = configs.map(
-          (config): FieldConfiguration<any> => {
-            const effectedField = fieldRule.effectedFields.find(
-              (field) => field.name === config.name
-            );
-
-            return effectedField
-              ? applyFieldRule(effectedField.actions, config, data)
-              : { ...config };
-          }
-        );
+      if (doesEventValueMatch(event, fieldRule)) {
+        newFieldConfigs = fieldConfigs.map((config) => {
+          return findEffectedSectionConfigurationApplyRule(
+            config,
+            fieldRule,
+            data ?? { [event.fieldName]: event.value }
+          );
+        });
       }
     });
 
-  return newConfigs;
+  return newFieldConfigs;
+}
+
+const doesEventValueMatch = (
+  event: FieldRuleProcessEvent,
+  fieldRule: FieldRule
+) => {
+  let eventMatches = false;
+  if (fieldRule.valueToMatch === '*') {
+    eventMatches = true;
+  } else if (isFunction(fieldRule.valueToMatch)) {
+    eventMatches = (fieldRule.valueToMatch as any)(event.value);
+  } else if (Array.isArray(fieldRule.valueToMatch)) {
+    eventMatches = !!(fieldRule.valueToMatch as any[]).find(
+      (value) => event.value === value
+    );
+  } else {
+    eventMatches = event.value === fieldRule.valueToMatch;
+  }
+
+  return (
+    eventMatches &&
+    (fieldRule.updateOn === event.action || fieldRule.updateOn === 'load')
+  );
+};
+
+const isSectionConfig = (config: FormFieldType) =>
+  !!(config as SectionConfiguration).fields;
+function findEffectedSectionConfigurationApplyRule(
+  config: FormFieldType,
+  fieldRule: FieldRule,
+  data: any
+): FormFieldType {
+  console.log('section effected');
+  return isSectionConfig(config)
+    ? {
+        ...config,
+        fields: (config as SectionConfiguration).fields?.map((config) =>
+          findEffectedSectionConfigurationApplyRule(
+            config as SectionConfiguration,
+            fieldRule,
+            data
+          )
+        ),
+      }
+    : findEffectedFieldConfigurationApplyRule(
+        fieldRule,
+        config as FieldConfiguration<any> | ReadonlyFieldConfiguration,
+        data
+      );
+}
+
+function findEffectedFieldConfigurationApplyRule(
+  fieldRule: FieldRule,
+  config: FieldConfiguration<any> | ReadonlyFieldConfiguration,
+  data: any
+): FieldConfiguration<any> | ReadonlyFieldConfiguration {
+  const effectedField = fieldRule.effectedFields.find((field) => {
+    console.log({ fieldRule, config });
+
+    return (
+      field.name === (config as FieldConfiguration<any>).controlConfig.name ||
+      field.name === (config as ReadonlyFieldConfiguration).inputId
+    );
+  });
+
+  return effectedField
+    ? applyFieldRule(
+        effectedField.actions,
+        config as FieldConfiguration<any>,
+        data
+      )
+    : { ...config };
 }
 
 function applyFieldRule(
   actions: FieldRuleAction[],
-  config: FieldConfiguration<any>,
+  config: FieldConfiguration<BaseFieldConfiguration>,
   data?: any
-): FieldConfiguration<any> {
+): FieldConfiguration<BaseFieldConfiguration> {
   const newConfig = { ...config, controlConfig: { ...config.controlConfig } };
-
-  function getActionValue(action: FieldRuleAction) {
-    return action.value
-      ? isFunction(action.value)
-        ? // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-          (action.value as Function)(data)
-        : typeof action.value === 'number'
-        ? +action.value
-        : action.value
-      : undefined;
-  }
-
   actions.forEach((action) => {
+    console.log({ action, data });
     switch (action.action) {
       case 'show':
         newConfig.isHidden = false;
@@ -77,10 +129,10 @@ function applyFieldRule(
         newConfig.isRequired = false;
         break;
       case 'disable':
-        newConfig.isDisabled = true;
+        newConfig.controlConfig.isDisabled = true;
         break;
       case 'enable':
-        newConfig.isDisabled = false;
+        newConfig.controlConfig.isDisabled = false;
         break;
       case 'readonly':
         newConfig.isReadonly = true;
@@ -89,42 +141,54 @@ function applyFieldRule(
         newConfig.isReadonly = false;
         break;
       case 'setMin':
-        (newConfig.controlConfig as NumberConfiguration).min =
-          getActionValue(action);
+        (newConfig.controlConfig as any).min = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setMax':
-        (newConfig.controlConfig as NumberConfiguration).max =
-          getActionValue(action);
+        (newConfig.controlConfig as any).max = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setMinDate':
-        (newConfig.controlConfig as DatePickerConfiguration).minDate =
-          getActionValue(action);
+        (newConfig.controlConfig as any).minDate = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setMaxDate':
-        (newConfig.controlConfig as DatePickerConfiguration).maxDate =
-          getActionValue(action);
+        (newConfig.controlConfig as any).maxDate = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setMinLength':
-        (newConfig.controlConfig as TextboxConfiguration).minLength =
-          getActionValue(action);
+        (newConfig.controlConfig as any).minLength = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setMaxLength':
-        (newConfig.controlConfig as TextboxConfiguration).maxLength =
-          getActionValue(action);
+        (newConfig.controlConfig as any).maxLength = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setValue':
-        newConfig.value = getActionValue(action);
+        newConfig.value = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
+        break;
+      case 'setLabel':
+        newConfig.label = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setPattern':
-        (newConfig.controlConfig as TextboxConfiguration).pattern =
-          getActionValue(action);
+        (newConfig.controlConfig as any).pattern = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       case 'setOptions':
-        (
-          newConfig.controlConfig as
-            | DropdownConfiguration
-            | CheckboxConfiguration
-        ).options = getActionValue(action);
+        (newConfig.controlConfig as any).options = isFunction(action.value)
+          ? action.value(data)
+          : action.value;
         break;
       default:
         break;
